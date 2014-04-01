2706beddff5226182d3902fe26854adb4ff114ac
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2014-04-01 22:56:03 +0100

    Modified the way picked files are loaded Added the ability to import data

--- inputs.py
+++ inputs.py
@@ -4,11 +4,24 @@
 @author: Dominic
 """
 
+import pandas as pd
 import cPickle as pickle
 
-def unpickleLog(fileName):
+from os import listdir
 
-    with open(fileName) as o:
-        results = pickle.load(o)
+def unpickleModels(folderName):
 
-    return results
+    for f in listdir(folderName):
+        if f.startswith("model_"):
+            fileName = folderName + f
+            with open(fileName) as o:
+                yield pickle.load(o)
+
+
+def readData(fileName):
+
+    # "../beads task stuff/Beads Data Exp 2 by CTA.xlsx"
+    xlf = pd.ExcelFile(fileName)
+    par = xlf.parse('Sheet2',header=0,index_col=0)
+
+    return par
