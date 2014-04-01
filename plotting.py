56a5151235a09201b0099e8c729240d4e3f507a9
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2014-04-01 07:48:23 +0100

    Commented out the contour plot in dim2VarDim Added the capacity in dataVsEvents to specify the Xand Y axis ranges

--- plotting.py
+++ plotting.py
@@ -10,8 +10,9 @@ import logging
 
 import matplotlib.pyplot as plt
 
-from numpy import array, meshgrid, fromfunction
+from numpy import array, meshgrid, fromfunction, arange
 from scipy import histogram, amin, amax
+from matplotlib.cm import get_cmap
 
 ### Define the different types of lines that will be plotted and their properties.
 # Symbols
@@ -38,8 +39,8 @@ wintermodCMap = {'blue': ((0.0, 1.0, 1.0), (0.0000000000000001, 1.0, 1.0), (1.0,
                'green': ((0.0, 1.0, 1.0), (0.0000000000000001, 1.0, 0.0), (1.0, 1.0, 1.0)),
                  'red': ((0.0, 1.0, 1.0), (0.0000000000000001, 1.0, 0.0), (1.0, 0.0, 0.0))}
 wintermod_cmap = matplotlib.colors.LinearSegmentedColormap('local_colormap_winter',wintermodCMap)
-local_cmap = wintermod_cmap
-#local_cmap = 'winter'
+#local_cmap = wintermod_cmap
+local_cmap = get_cmap('winter')
 
 origin = 'lower'
 #origin = 'upper'
@@ -55,7 +56,7 @@ def varDynamics(params, paramVals, decisionTimes):
     elif lparams == 2:
         X,Y = meshgrid(*paramVals)
         decisionTimes = array(decisionTimes)
-        decisionTimes.reshape(X.shape)
+        decisionTimes  = decisionTimes.reshape(X.shape)
         fig = dim2VarDim(X,Y,decisionTimes, params[0], params[1])
 
     elif lparams == 3:
@@ -92,27 +93,59 @@ def dim1VarDim(X,Y, varXLabel):
 def dim2VarDim(X,Y,Z, varXLabel, varYLabel):
     """
     """
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
     minZ = amin(Z)-1
     maxZ = amax(Z)+2
-    levels = range(minZ,maxZ)
-    CS = plt.contour(X, Y, Z, levels,
-                        colors = local_cmap,
-                        origin=origin,
-                        linewidths = 3,
-                        extend='both')
-    plt.clabel(CS, fmt = '%2.1f', colors = 'w', fontsize=14)
+    levels = arange(minZ,maxZ,2)
+    area = [amin(X),amax(X),amin(Y),amax(Y)]
+
+#    CS = plt.contour(X,Y, Z, levels,
+#                     origin='lower',
+#                     colors = 'w',
+#                     linewidths=2,
+#                     extent=area,
+#                     extend = 'both')
+#    plt.clabel(CS, levels[1::2],  # label every second level
+#               inline=1,
+#               fmt='%2.1f',
+#               fontsize=14,
+#               colors = 'w')
     plt.xlabel(varXLabel)
     plt.ylabel(varYLabel)
     plt.title('Parameter reaction times')
 
 #    CS.cmap.set_under('yellow')
 #    CS.cmap.set_over('cyan')
+#    CS.cmap.set_bad("red")
+#
+#    CB = plt.colorbar(CS, shrink=0.8, extend='both')
+#    CB.set_label("Reaction time")
 
-    col = plt.colorbar()
-    col.set_label("Reaction time")
+    im = plt.imshow(Z, interpolation='nearest',
+                    origin='lower',
+                    cmap=local_cmap,
+                    extent=area,
+                    aspect='auto' )
+    CBI = plt.colorbar(im, orientation='horizontal', shrink=0.8)
+
+# Stuff taken from a contour template. Could be useful.
+
+#    #Thicken the zero contour.
+#    zc = CS.collections[6]
+#    plt.setp(zc, linewidth=4)
+
+#    #plt.hot()  # Now change the colormap for the contour lines and colorbar
+#    plt.flag()
+
+
+#    # This makes the original colorbar look a bit out of place,
+#    # so let's improve its position.
+#    l,b,w,h = plt.gca().get_position().bounds
+#    ll,bb,ww,hh = CB.ax.get_position().bounds
+#    CB.ax.set_position([ll, b+0.1*h, ww, h*0.8])
 
     return fig
 
@@ -130,6 +163,11 @@ def dataVsEvents(data,events,labels,eventLabel,axisLabels):
     y2Label = axisLabels.pop("y2Label","Event value")
     title = axisLabels.pop("title","")
 
+    yMin = axisLabels.pop("yMin",None)
+    yMax = axisLabels.pop("yMax",None)
+    xMin = axisLabels.pop("xMin",None)
+    xMax = axisLabels.pop("xMax",None)
+
     eventX = fromfunction(lambda i: i+0.5, (len(events),))
 
 
@@ -146,6 +184,19 @@ def dataVsEvents(data,events,labels,eventLabel,axisLabels):
     ax.set_ylabel(yLabel)
     axb.set_ylabel(y2Label)
     ax.set_title(title)
+
+    yLim = list(ax.get_ylim())
+    if yMin != None:
+        yLim[0] = yMin
+    if yMax != None:
+        yLim[1] = yMax
+    ax.set_ylim(yLim)
+    xLim = list(ax.get_xlim())
+    if xMin != None:
+        xLim[0] = xMin
+    if xMax != None:
+        xLim[1] = xMax
+    ax.set_xlim(xLim)
 
     lines1, pltLables1 = ax.get_legend_handles_labels()
     lines2, pltLables2 = axb.get_legend_handles_labels()
