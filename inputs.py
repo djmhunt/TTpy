a629cb2b66627016ecd449479be527a235764197
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2014-04-10 17:59:35 +0100

    Added unpickleSimDescription
    Modified unpickleModels to read from the correct folder and return a label with the data

--- inputs.py
+++ inputs.py
@@ -7,15 +7,32 @@
 import pandas as pd
 import cPickle as pickle
 
+import logging
+
 from os import listdir
+from os.path import isfile, exists
 
 def unpickleModels(folderName):
 
-    for f in listdir(folderName):
+    pickleFolderName = folderName + 'Pickle\\'
+    for f in listdir(pickleFolderName):
         if f.startswith("model_"):
-            fileName = folderName + f
+            fileName = pickleFolderName + f
             with open(fileName) as o:
-                yield pickle.load(o)
+                label = f[6:][:-4]
+                data = pickle.load(o)
+                yield label, data
+
+def unpickleSimDescription(folderName):
+
+    fileName = folderName + 'Pickle\\simStore.pkl'
+    if isfile(fileName):
+        with open(fileName) as o:
+            data = pickle.load(o)
+            return data
+    else:
+        logger1 = logging.getLogger('Inputs')
+        logger1.warning("The simulation records were not kept. Check how things were performed")
 
 
 def readData(fileName):
