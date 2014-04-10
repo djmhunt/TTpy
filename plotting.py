72404a4c482fb8f1bab1bcc045e4c8a861ab93ed
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2014-04-10 17:51:56 +0100

    Removed references to reaction times and put in place references to time to decision
    Added ability to define the size of axes for each plot
    
    Heavily rewritten dim2VarDim

--- plotting.py
+++ plotting.py
@@ -10,9 +10,13 @@ import logging
 
 import matplotlib.pyplot as plt
 
-from numpy import array, meshgrid, fromfunction, arange
+from numpy import array, meshgrid, fromfunction, arange,linspace, sort
 from scipy import histogram, amin, amax
+from scipy.interpolate import griddata
 from matplotlib.cm import get_cmap
+from itertools import izip
+from bisect import bisect_right
+
 
 ### Define the different types of lines that will be plotted and their properties.
 # Symbols
@@ -39,8 +43,8 @@ wintermodCMap = {'blue': ((0.0, 1.0, 1.0), (0.0000000000000001, 1.0, 1.0), (1.0,
                'green': ((0.0, 1.0, 1.0), (0.0000000000000001, 1.0, 0.0), (1.0, 1.0, 1.0)),
                  'red': ((0.0, 1.0, 1.0), (0.0000000000000001, 1.0, 0.0), (1.0, 0.0, 0.0))}
 wintermod_cmap = matplotlib.colors.LinearSegmentedColormap('local_colormap_winter',wintermodCMap)
-#local_cmap = wintermod_cmap
-local_cmap = get_cmap('winter')
+local_cmap = wintermod_cmap
+#local_cmap = get_cmap('winter')
 
 origin = 'lower'
 #origin = 'upper'
@@ -54,10 +58,7 @@ def varDynamics(params, paramVals, decisionTimes):
         fig = dim1VarDim(paramVals[0],decisionTimes, params[0])
 
     elif lparams == 2:
-        X,Y = meshgrid(*paramVals)
-        decisionTimes = array(decisionTimes)
-        decisionTimes  = decisionTimes.reshape(X.shape)
-        fig = dim2VarDim(X,Y,decisionTimes, params[0], params[1])
+        fig = dim2VarDim(paramVals[0],paramVals[1],decisionTimes, params[0], params[1])
 
     elif lparams == 3:
         fig = None
@@ -82,70 +83,108 @@ def dim1VarDim(X,Y, varXLabel):
         ax.set_ylim((minVal,maxVal))
     else:
         logger1 = logging.getLogger('Plots')
-        logger1.warning("There is no variation in the parameter reaction times")
+        logger1.warning("There is no variation in the time to decision across parameters")
 
     plt.xlabel(varXLabel)
-    plt.ylabel("Reaction")
-    plt.title("Parameter reaction times")
+    plt.ylabel("Time to decision")
+    plt.title("Time to decision across parameters")
+
+    plt.tight_layout(pad=0.6, w_pad=0.5, h_pad=1.0)
 
     return fig
 
-def dim2VarDim(X,Y,Z, varXLabel, varYLabel):
+def dim2VarDim(x,y,z, varXLabel, varYLabel):
     """
+
+    Todo: Split out the actual plotting so contour plots do not interfere with heatmaps.
+
+    Put in a scatter plot: plt.scatter(theta,beta,s=beadTotal)
+
+    Look in to fatfonts
     """
+
+    X,Y = meshgrid(x,y)
+    z = array(z)
+    Z = z.reshape(X.shape)
+
+    xMin = amin(X)
+    xMax = amax(X)
+    yMin = amin(Y)
+    yMax = amax(Y)
+    if yMin == yMax:
+        logger1 = logging.getLogger('Plots')
+        logger1.warning("There is no variation in the time to decision across parameters")
+
+    minZ = 0
+    maxZ = amax(z)
+    if maxZ == minZ:
+        maxZ += 1
+    zSorted = sort(z)
+    i = bisect_right(zSorted, 0)
+    if i != len(zSorted):
+        dz = zSorted[i]
+        zJumps = (maxZ - minZ)/dz + 2
+        levels = linspace(minZ,maxZ+dz,zJumps)
+    else:
+        levels = arange(maxZ+1)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    minZ = amin(Z)-1
-    maxZ = amax(Z)+2
-    levels = arange(minZ,maxZ,2)
-    area = [amin(X),amax(X),amin(Y),amax(Y)]
-
-#    CS = plt.contour(X,Y, Z, levels,
-#                     origin='lower',
-#                     colors = 'w',
-#                     linewidths=2,
-#                     extent=area,
-#                     extend = 'both')
-#    plt.clabel(CS, levels[1::2],  # label every second level
-#               inline=1,
-#               fmt='%2.1f',
-#               fontsize=14,
-#               colors = 'w')
-    plt.xlabel(varXLabel)
-    plt.ylabel(varYLabel)
-    plt.title('Parameter reaction times')
+    CS = plt.contour(X,Y, Z, levels,
+                     origin='lower',
+                     colors = 'k',
+                     linewidths=2,
+                     extent=[xMin,xMax,yMin, yMax],
+                     extend = 'both')
+    plt.clabel(CS, levels,#[1::2],  # label every second level
+               inline=1,
+               fmt='%2.1f',
+               fontsize=14,
+               colors = 'k')
 
 #    CS.cmap.set_under('yellow')
 #    CS.cmap.set_over('cyan')
-#    CS.cmap.set_bad("red")
-#
-#    CB = plt.colorbar(CS, shrink=0.8, extend='both')
-#    CB.set_label("Reaction time")
+    CS.cmap.set_bad("red")
 
-    im = plt.imshow(Z, interpolation='nearest',
+    CB = plt.colorbar(CS, shrink=0.8, extend='both')
+    CB.set_label("Time to decision")
+
+    dx = amin(x[1:]-x[:-1])/2.0
+    dy = amin(y[1:]-y[:-1])/2.0
+    xJumps = (xMax - xMin)/dx + 2
+    yJumps = (yMax - yMin)/dy + 2
+
+    xMinIm = xMin - dx
+    xMaxIm = xMax + dx
+    yMinIm = yMin - dy
+    yMaxIm = yMax + dy
+
+    Xfleshed,Yfleshed = meshgrid(linspace(xMinIm,xMaxIm,xJumps),linspace(yMinIm,yMaxIm,yJumps))
+
+    zPoints = [(a,b) for a,b in izip(X.flatten(),Y.flatten())]
+    gridZ = griddata(zPoints, z, (Xfleshed, Yfleshed), method='nearest')
+
+#    qm = plt.pcolormesh(gridZ, cmap = local_cmap)
+    im = plt.imshow(gridZ, interpolation='nearest',
                     origin='lower',
                     cmap=local_cmap,
-                    extent=area,
-                    aspect='auto' )
+                    extent=[xMinIm,xMaxIm,yMinIm,yMaxIm],
+                    vmin = minZ,
+                    aspect='auto')
     CBI = plt.colorbar(im, orientation='horizontal', shrink=0.8)
+    CBI.set_label("Time to decision")
 
-# Stuff taken from a contour template. Could be useful.
+    plt.xlabel(varXLabel)
+    plt.ylabel(varYLabel)
+    plt.title('Time to decision across parameters')
 
-#    #Thicken the zero contour.
-#    zc = CS.collections[6]
-#    plt.setp(zc, linewidth=4)
+    # Improving the position of the contour bar legend
+    l,b,w,h = plt.gca().get_position().bounds
+    ll,bb,ww,hh = CB.ax.get_position().bounds
+    CB.ax.set_position([ll, b+0.1*h, ww, h*0.8])
 
-#    #plt.hot()  # Now change the colormap for the contour lines and colorbar
-#    plt.flag()
-
-
-#    # This makes the original colorbar look a bit out of place,
-#    # so let's improve its position.
-#    l,b,w,h = plt.gca().get_position().bounds
-#    ll,bb,ww,hh = CB.ax.get_position().bounds
-#    CB.ax.set_position([ll, b+0.1*h, ww, h*0.8])
+    plt.tight_layout(pad=0.6, w_pad=0.5, h_pad=1.0)
 
     return fig
 
@@ -163,10 +202,10 @@ def dataVsEvents(data,events,labels,eventLabel,axisLabels):
     y2Label = axisLabels.pop("y2Label","Event value")
     title = axisLabels.pop("title","")
 
-    yMin = axisLabels.pop("yMin",None)
-    yMax = axisLabels.pop("yMax",None)
-    xMin = axisLabels.pop("xMin",None)
-    xMax = axisLabels.pop("xMax",None)
+    yMin = axisLabels.pop("yMin",0)
+    yMax = axisLabels.pop("yMax",amax(data))
+    xMin = axisLabels.pop("xMin",0)
+    xMax = axisLabels.pop("xMax",len(data[0]))
 
     eventX = fromfunction(lambda i: i+0.5, (len(events),))
 
@@ -185,24 +224,26 @@ def dataVsEvents(data,events,labels,eventLabel,axisLabels):
     axb.set_ylabel(y2Label)
     ax.set_title(title)
 
-    yLim = list(ax.get_ylim())
-    if yMin != None:
-        yLim[0] = yMin
-    if yMax != None:
-        yLim[1] = yMax
-    ax.set_ylim(yLim)
-    xLim = list(ax.get_xlim())
-    if xMin != None:
-        xLim[0] = xMin
-    if xMax != None:
-        xLim[1] = xMax
-    ax.set_xlim(xLim)
+#    yLim = list(ax.get_ylim())
+#    if yMin != None:
+#        yLim[0] = yMin
+#    if yMax != None:
+#        yLim[1] = yMax
+#    ax.set_ylim(yLim)
+#    xLim = list(ax.get_xlim())
+#    if xMin != None:
+#        xLim[0] = xMin
+#    if xMax != None:
+#        xLim[1] = xMax
+#    ax.set_xlim(xLim)
 
     lines1, pltLables1 = ax.get_legend_handles_labels()
     lines2, pltLables2 = axb.get_legend_handles_labels()
     pltLables = pltLables1 + pltLables2
     lines = lines1 + lines2
     leg = ax.legend(lines, pltLables,loc = 'best', fancybox=True)
+
+    plt.tight_layout(pad=0.6, w_pad=0.5, h_pad=1.0)
 
     return fig
 
@@ -211,19 +252,17 @@ def dataSpectrumVsEvents(data,events,labels,eventLabel,axisLabels):
 
     printResourcesSpectrum(data,bins)"""
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+
 
     xLabel = axisLabels.pop("xLabel","Events")
     yLabel = axisLabels.pop("yLabel","Density across parameter range")
     y2Label = axisLabels.pop("y2Label","Event value")
     title = axisLabels.pop("title","")
 
-    maxVal = amax(data)
-    minVal = amin(data)
-
-    if minVal > 0:
-        minVal = 0
+    minVal = axisLabels.pop("yMin",0)
+    maxVal = axisLabels.pop("yMax",amax(data))
+    xMin = axisLabels.pop("xMin",0)
+    xMax = axisLabels.pop("xMax",len(data[0]))
 
     bins = 25
 
@@ -236,7 +275,15 @@ def dataSpectrumVsEvents(data,events,labels,eventLabel,axisLabels):
 
     plotData = array([d[0] for d in histData])
 
-    plt.imshow(plotData.T, interpolation='nearest', cmap=wintermod_cmap, origin='lower', extent=[0,len(plotData),minVal,maxVal], aspect='auto' )
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    plt.imshow(plotData.T,
+               interpolation='nearest',
+               cmap=wintermod_cmap,
+               origin='lower',
+               extent=[xMin,xMax,minVal,maxVal],
+               aspect='auto' )
     col = plt.colorbar(orientation='horizontal')
     col.set_label("Probability density")
 
@@ -250,6 +297,36 @@ def dataSpectrumVsEvents(data,events,labels,eventLabel,axisLabels):
     axb.set_ylabel(y2Label)
     ax.set_title(title)
 
-
+    plt.tight_layout(pad=0.6, w_pad=0.5, h_pad=1.0)
 
     return fig
+
+if __name__ == '__main__':
+
+#    X = array([[1, 2, 4], [1, 2, 4], [1, 2, 4]])
+    x = array([1,2,4])
+
+#    Y = array([[ 0.1,  0.1,  0.1], [ 0.2,  0.2,  0.2], [ 0.3,  0.3,  0.3]])
+    y = array([0.1, 0.2, 0.3])
+
+#    Z = array([[2, 1, 1], [0, 2, 1], [0, 0, 1]])
+    z = array([1,2,3,2,3,4,3,4,5])
+
+    fig = dim2VarDim(x,y,z, 'TestX', 'TestY')
+
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111)
+#
+#    minZ = amin(Z)-1
+#    maxZ = amax(Z)+2
+#    levels = arange(minZ,maxZ,2)
+#    area = [amin(X),amax(X),amin(Y),amax(Y)]
+#
+#    qm = plt.pcolormesh(X, Y, Z,
+#                   cmap = local_cmap)
+#    CBI = plt.colorbar(qm, orientation='horizontal', shrink=0.8)
+#    CBI.set_label("Time to decision")
+
+#    plt.gcf().set_size_inches(6, 6)
+
+    plt.show()
