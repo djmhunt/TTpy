4240a4405f08a1a1694b6a912144e46cc7d8a4d8
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2015-02-11 19:07:02 +0000

    Added a new model and experiment Some bugs still to iron out

---
+++ experiment/pavlov.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Dominic Hunt
+
+:Reference: Value and prediction error in medial frontal cortex: integrating the single-unit and systems levels of analysis.
+                `Silvetti, M., Seurinck, R., & Verguts, T. (2011)`. 
+                Frontiers in Human Neuroscience, 5(August), 75. 
+                doi:10.3389/fnhum.2011.00075
+"""
+
+from __future__ import division
+
+from numpy import array, ones, zeros, concatenate
+from numpy.random import choice, random
+
+from experiment import experiment
+from experimentPlot import experimentPlot
+
+class pavlov(experiment):
+
+    """
+    Based on the Silvetti et al 2011 paper `"Value and prediction error in 
+    medial frontal cortex: integrating the single-unit and systems levels of 
+    analysis."`
+    
+    Many methods are inherited from the experiment.experiment.experiment class.
+    Refer to its documentation for missing methods.
+    
+    Attributes
+    ----------
+    Name : string
+        The name of the class used when recording what has been used.
+    
+    Parameters
+    ----------
+    rewMag : float, optional
+        The size of the stimulus. Default 4
+    rewProb : array of floats, optional
+        The probabilities of each stimulus producing a reward. 
+        Default [0.85,0.33]
+    stimMag : float, optional
+        The size of the stimulus. Default 1
+    stimDur : int, optional
+        The duration, in tens of ms, that the stimulus is produced for. This
+        should be longer than rewDur since rewDur is set to end when stimDur
+        ends. Default 200
+    rewDur : int, optional
+        The duration, in tens of ms, that the reward is produced for.
+        Default 40
+    simDur : int, optional
+        The duration, in tens of ms, that each stimulus event is run for.
+        Default 300
+    stimRepeats : int, optional
+        The number of times a stimulus is introduced. Default 72
+    
+    """
+
+    Name = "pavlov"
+
+    def reset(self):
+        """ 
+        Creates a new experiment instance
+
+        Returns
+        -------
+        self : The cleaned up object instance
+        """
+
+        kwargs = self.kwargs.copy()
+        
+        self.rewMag = kwargs.pop('rewMag',4)
+        self.rewProb = kwargs.pop('rewProb',array([0.87,0.33]))
+        self.stimMag = kwargs.pop('stimMag',1)
+        self.stimDur = kwargs.pop('stimDur',20)#200) # Stimulus duration
+        self.rewDur = kwargs.pop('rewDur',4)#40) #duration of reward
+        self.simLen = kwargs.pop('simDur',30)#300) # the length of the simulation
+        self.stimRepeats = kwargs.pop('stimRepeats',7)#72) # The number of learning runs
+#        simLoop = kwargs.pop('simLoopLen',100) #The number of learning loops are run
+        
+        self.index = -1
+
+        self.plotArgs = kwargs.pop('plotArgs',{})
+
+        self.parameters = {"Name": self.Name,
+                           "rewMag": self.rewMag,
+                           "rewProb": self.rewProb,
+                           "stimMag": self.stimMag,
+                           "stimDur": self.stimDur,
+                           "rewDur": self.rewDur,
+                           "simLen": self.simLen,
+                           "stimRepeats": self.stimRepeats}
+        
+        self.cSet, self.stimChoice = self._getStim(self.stimRepeats, self.stimMag)
+        self.rewSigSet, self.rewVals = self._getRew(self.stimChoice, self.simLen, self.stimRepeats, self.stimDur, self.rewDur, self.rewMag, self.rewProb)
+        
+        self.recActions = []        
+        
+        return self
+
+    def next(self):
+        """
+        Produces the next bead for the iterator
+        
+        Returns
+        -------
+        c : list of floats
+            Contains the inputs for each of the stimuli
+        rewSig : list of lists of floats
+            Each list contains the rewards at each time
+        stimDur : int
+            
+        
+        Raises
+        ------
+        StopIteration
+        """
+
+        self.index += 1
+
+        if self.index == self.stimRepeats:
+            raise StopIteration
+            
+        c = self.cSet[self.index]
+        rewSig = self.rewSigSet[self.index]
+
+        return c, rewSig, self.stimDur
+
+    def receiveAction(self,action):
+        """
+        Receives the next action from the participant
+        
+        Parameters
+        ----------
+        action : ignored
+            The action is only stored, not used.
+        """
+
+        self.action = action
+        
+        self.storeState()
+
+    def outputEvolution(self):
+        """ 
+        Plots and saves files containing all the relavent data for this
+        experiment run 
+        
+        Returns
+        -------
+        results : dictionary
+            Contains the class paramters as well as the other useful data
+        """
+
+        results = self.parameters
+        
+        results["choices"] = self.cSet
+        results["stimuli"] = self.stimChoice
+        results["rewards"] = self.rewVals
+        results["actions"] = self.recActions
+        
+        return results
+
+    def storeState(self):
+        """ 
+        Stores the state of all the important variables so that they can be
+        output later 
+        """
+        
+        self.recActions.append(self.action)
+
+        
+    def _getStim(self, stimRepeats, stimMag):
+        stimChoice = choice([0,1],size = (stimRepeats,1))
+        cSet = array([[1-sc[0],sc[0]] for sc in stimChoice])*stimMag
+        
+        return cSet, stimChoice
+        
+    def _getRew(self, stimChoice, simLen, stimRepeats, stimDur, rewDur, rewMag, rewProb):
+    
+        rewVals = (random((stimRepeats,1)) < rewProb[stimChoice])*rewMag
+        rewSig1 = zeros((stimRepeats,stimDur-rewDur))
+        rewSig2 = ones((stimRepeats,rewDur))*rewVals
+        rewSig3 = zeros((stimRepeats,simLen-stimDur))
+        rewSigSet = concatenate((rewSig1,rewSig2,rewSig3),1)
+        
+        return rewSigSet, rewVals
+        
+def pavlovStimTemporal(eventDur):
+    """
+    Passes the pavlov stimuli to models that cope with stimuli and rewards 
+    that have a duration.
+    
+    Parameters
+    ----------
+    eventDur : int
+        The length of each event. Must be longer than stimDur and be 
+        absolutely the same length as simDur.
+    
+    """
+    
+    def pavlovStim(event):
+        
+        cStim = event[0]
+        rewSig = event[1]
+        stimDur = event[2]
+    
+        cStimZeros = zeros((len(cStim)))
+        
+        for t, r in enumerate(rewSig):
+            
+            if t < stimDur:
+                c = cStim
+            else:
+                c = cStimZeros 
+                
+            yield t, c, r
+            
+    pavlovStim.Name = "pavlovStimTemporal"
+    pavlovStim.simDur = eventDur
+            
+    return pavlovStim
+        
+        
---
+++ model/RVPM.py
@@ -0,0 +1,365 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Dominic Hunt
+
+:Reference: Value and prediction error in medial frontal cortex: integrating the single-unit and systems levels of analysis.
+                `Silvetti, M., Seurinck, R., & Verguts, T. (2011)`. 
+                Frontiers in Human Neuroscience, 5(August), 75. 
+                doi:10.3389/fnhum.2011.00075
+"""
+from __future__ import division
+
+import logging
+
+from numpy import exp, zeros, array, amax, dot, argmax, mean, square
+from random import choice
+from collections import defaultdict
+
+from model import model
+from modelPlot import modelPlot
+from modelSetPlot import modelSetPlot
+from utils import listMerge, mergeDatasets
+from plotting import lineplot
+
+class RVPM(model):
+
+    """The reward value and prediction model
+    
+    Attributes
+    ----------
+    Name : string
+        The name of the class used when recording what has been used.
+    
+    Parameters
+    ----------
+    alpha : float, optional
+        Learning rate parameter for updating the cue-value weights. Default 0.3
+    gamma : float, optional
+        It is a time constant that controls how quickly the neural units 
+        (modeled as dynamical systems) respond to external inputs. Default 0.1
+    theta : float, optional
+        Used for gamma from the paper. Theta is used in other models for the 
+        same purpose, so is added here as another option. Gamma will override
+        theta.
+    zeta : float, optional
+        Regulates the ratio between the power (amplitude) of expectation 
+        relative to delta units. Default 2
+    w : array, optional
+        The stimulus weights. Default [0.01,0.01]
+    tau : float, optional
+        The timing signal mean. Default 160
+    z : float, optional
+        The timing signal spread. Default 20 
+    averaging : int, optional
+        The number of stimuli recorded from the beginning and end of each 
+        training set. Default is 3
+    stimFunc : function, optional
+        The function that transforms the stimulus into a form the model can 
+        understand and a string to identify it later. Default is blankStim
+    decFunc : function, optional
+        The function that takes the internal values of the model and turns them
+        in to a decision. Default is basicDecision
+    """
+
+    Name = "RVPM"
+
+    def __init__(self,**kwargs):
+
+        self.alpha = kwargs.pop('alpha',0.005)
+        defaultGamma = 0.1
+        self.gamma = kwargs.pop('gamma',defaultGamma)
+        if self.gamma == defaultGamma:
+            self.gamma = kwargs.pop('theta',defaultGamma)
+        self.w = kwargs.pop('w',array([0.01,0.01]))
+        self.zeta = kwargs.pop('zeta',2)
+        self.tau = kwargs.pop('tau',160)
+        self.z = kwargs.pop('z',100)
+        self.averaging = kwargs.pop('averaging',3)
+        
+        self.stimFunc = kwargs.pop('stimFunc',blankStim())
+        self.decisionFunc = kwargs.pop('decFunc',basicDecision())
+        
+        self.T = 0 # Timing sgnal value
+        self.c = 0 # The stimuli
+        self.r = 0 # Reward value
+        self.V = 0 #Reward prediction unit
+        self.deltaP = 0 # positivie prediction error unit
+        self.deltaM = 0 # negative prediction error unit
+        self.TSN = 0 # Temporaly shifted neuron
+
+        self.parameters = {"Name": self.Name,
+                           "theta": self.gamma,
+                           "alpha": self.alpha,
+                           "wInit": self.w,
+                           "zeta" : self.zeta,
+                           "tau" : self.tau,
+                           "z" : self.z,
+                           "averaging" : self.averaging,
+                           "stimFunc" : self.stimFunc.Name,
+                           "decFunc" : self.decisionFunc.Name}
+
+        self.currAction = None
+        self.lastObs = False
+
+        # Recorded information
+
+        self._storeSetup()
+
+    def action(self):
+        """
+        Returns
+        -------
+        action : integer or None
+        """
+        self.currAction = self.decisionFunc(self.TSN)
+
+        self.storeState()
+
+        return self.currAction
+        
+    def outputEvolution(self):
+        """ Returns all the relevent data for this model 
+        
+        Returns
+        -------
+        results : dict
+            The dictionary contains a series of keys including Name, 
+            Probabilities, Actions and Events.
+        """
+
+        av = self.averaging
+
+        results = self.parameters
+        
+        for k,v in self.generalStore.iteritems():
+            
+            results[k + '_early'] = v[:av]
+            results[k + '_late'] = v[-av:]
+
+        return results
+
+    def _update(self,event,instance):
+        """Processes updates to new actions"""
+
+        if instance == 'obs':
+
+            #Calculate the new information
+            self._processEvent(event)
+
+            self.lastObs = True
+
+        elif instance == 'reac':
+
+            if self.lastObs:
+
+                self.lastObs = False
+
+            else:
+                
+                #Calculate the new information
+                self._processEvent(event)
+
+    def storeState(self):
+        """ 
+        Stores the state of all the important variables so that they can be
+        accessed later 
+        """
+
+        self.recAction.append(self.currAction)
+        self._updateGeneralStore()
+
+    def _processEvent(self,event):
+        
+        for t,c,r in self.stimFunc(event):
+            
+            self.c = c
+            self.r = r
+            self._processStim(t,c,r)
+            self._updateEventStore()
+            
+    def _processStim(self,t,c,r):
+        
+        T = self._timeSigMag(t)
+
+        dV = self._vUpdate(self.w,self.V,c)
+        self.V = self.V + dV
+        ddeltaP = self._deltaPUpdate(self.V,self.deltaP,r)
+        self.deltaP = self.deltaP + ddeltaP
+
+        ddeltaM = self._deltaMUpdate(self.V,self.deltaM,T,r)
+        self.deltaM = self.deltaM + ddeltaM
+
+        self.TSN = self._tsnUpdate(dV,ddeltaP,ddeltaM)
+        
+        self.w = self._wNew(self.w,self.V,self.deltaP,self.deltaM,c)
+            
+    def _wNew(self,w,V,deltaP,deltaM,c):
+        new = w + self.alpha*c*V*(deltaP - deltaM)
+        return new
+    
+    def _vUpdate(self,w,V,c):
+        gamma = self.gamma
+        new = -gamma*V + gamma*amax([0,dot(w,c)])
+        return new
+        
+    def _deltaPUpdate(self,V,deltaP,r):
+        gamma = self.gamma
+        new = -gamma*deltaP + gamma*amax([0,r-self.zeta*V])
+        return new
+        
+    def _timeSigMag(self,t):
+        signal = exp((-(t-self.tau)**2)/square(self.z))
+        return signal
+        
+    def _deltaMUpdate(self,V,deltaM,T,r):
+        gamma = self.gamma
+        new = -gamma*deltaM + gamma*T*amax([0,self.zeta*V-r])
+        return new
+        
+    def _tsnUpdate(self,dV,ddeltaP,ddeltaM):
+        signal = amax([0,self.zeta*dV]) + amax([0,ddeltaP]) - amax([0,ddeltaM])
+        return signal
+        
+    def _storeSetup(self):
+        
+        self.eventStore = {}
+        self.eventStore["T"] = []
+        self.eventStore["V"] = []
+        self.eventStore["DP"] = []
+        self.eventStore["DM"] = []
+        self.eventStore["TSN"] = []
+        self.eventStore["stim"] = []
+        self.eventStore["rew"] = []
+        self.eventStore["w"] = []
+        
+        self._generalStoreSetup()
+        
+    def _generalStoreSetup(self):
+        
+        self.recAction = []
+        self.generalStore = {}
+        
+        for k in self.eventStore.iterkeys():
+            self.generalStore[k] = []
+        
+    def _updateEventStore(self):
+        
+        self.eventStore["T"].append(self.T)
+        self.eventStore["V"].append(self.V)
+        self.eventStore["DP"].append(self.deltaP)
+        self.eventStore["DM"].append(self.deltaM)
+        self.eventStore["TSN"].append(self.TSN)
+        self.eventStore["stim"].append(self.c)
+        self.eventStore["rew"].append(self.r)
+        self.eventStore["w"].append(self.w)
+        
+        
+    def _updateGeneralStore(self):
+        
+        for k,v in self.eventStore.iteritems():
+            self.generalStore[k].append(array(v))
+            
+        for k in self.eventStore.iterkeys():
+            self.eventStore[k] = []
+            
+    class modelSetPlot(modelSetPlot):
+        
+        """Class for the creation of plots relevant to the model set"""
+
+        def _figSets(self):
+            """ Contains all the figures """
+
+            self.figSets = []
+
+            # Create all the plots and place them in in a list to be iterated
+
+            self._processData()
+            fig = self.avResponse("V")
+            self.figSets.append(('VResponse',fig))
+            fig = self.avResponse("DP")
+            self.figSets.append(('dPResponse',fig))
+            fig = self.avResponse("DM")
+            self.figSets.append(('dMResponse',fig))
+            fig = self.avResponse("TSN")
+            self.figSets.append(('TSNResponse',fig))
+            fig = self.avResponse("w")
+            self.figSets.append(('wResponse',fig))
+            
+        def _processData(self):
+            
+            self.modelData = mergeDatasets(self.modelStore, extend = True)
+            
+            averagedData = defaultdict(dict)
+
+            for t in ["_early","_late"]:
+                cmax = [c.argmax() for c in self.modelData["stim"+t]]
+                
+                for key in ["V","DP","DM","TSN"]:#,"w"]:
+                    averagedData[key][key+t] = self._dataAverage(self.modelData[key+t], cmax)
+                    
+            self.modelAverages = averagedData
+            
+        def _dataAverage(self, data, cmax):
+            
+            cLists = [[] for i in xrange(max(cmax)+1)]
+            
+            for i, cm in enumerate(cmax):
+                cLists[cm].append(data[i])
+                
+            meanVals = [mean(i, axis=0) for i in cLists]
+            
+            return meanVals
+            
+        def avResponse(self,key):
+            """
+            The averaged response from different parts of the model in the form
+            of figure 1 from the paper
+            """
+            data = self.modelAverages[key]
+            
+            dataKeys = data.keys()
+            dataStim = [i for i in xrange(len(data[dataKeys[0]]))]
+            
+            Y = array(range(len(data[dataKeys[0]][0])))
+            
+            labels = ["c=" + j + " " + i for i, j in listMerge(dataKeys,dataStim)]
+            
+            plotData = [data[i][int(j)] for i, j in listMerge(dataKeys,dataStim)]
+            
+            axisLabels = {"title":"Untitled"}
+            axisLabels["xLabel"] = r"$t/10 \textrm{ms}$"
+            axisLabels["yLabel"] = key
+#            axisLabels["yMax"] = 0
+#            axisLabels["yMin"] = -0.5
+
+            fig = lineplot(Y,plotData,labels,axisLabels)
+
+            return fig
+
+def blankStim(): 
+    
+    def blankStimFunc(event):
+    
+        eventIter = argmax((len(e) for e in event))
+            
+        
+        for t, c in enumerate(event[eventIter]):
+                
+            yield t, c, 0
+            
+    blankStimFunc.Name = "blankStim"
+    
+    return blankStimFunc
+        
+def basicDecision():
+    
+    def basicDecisionFunc(prob):
+                
+        return None
+        
+    basicDecisionFunc.Name = "basicDecision"
+    
+    return basicDecisionFunc
+            
+
+
