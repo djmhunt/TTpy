42353ac46222c241b5bd0fec2ab114c851fde3e4
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2015-03-20 17:12:26 +0000

    Placed some functions in fitAlg and added some documentation

--- fitting/fitters/leastsq.py
+++ fitting/fitters/leastsq.py
@@ -15,13 +15,34 @@ class leastsq(fitAlg):
     
     Not properly developed and will not be documented until upgrade
 
-
+    Parameters
+    ----------
+    fitQualFunc : function, optional
+        The function used to calculate the quality of the fit. The value it 
+        returns proivides the fitter with its fitting guide. Default ``fitAlg.null``
+    numStartPoints : int, optional
+        The number of starting points generated for each parameter.
+        Default 4
+        
+    Attributes
+    ----------
+    Name: string
+        The name of the fitting method    
+    
+    See Also
+    --------
+    fitting.fitters.fitAlg.fitAlg : The general fitting method class, from 
+                                    which this one inherits
+    fitting.fit.fit : The general fitting framework class
+    scipy.optimise.leastsq : The fitting class this wraps around
 
     """
 
     Name = 'leastsq'
 
-    def __init__(self,fitQualFunc = None):
+    def __init__(self,fitQualFunc = None, numStartPoints = 4):
+        
+        self.numStartPoints = numStartPoints
 
         if fitQualFunc == "-2log":
             self.fitness = self.logprob
@@ -31,21 +52,6 @@ class leastsq(fitAlg):
         self.fitInfo = {'Name':self.Name,
                         'shaper': fitQualFunc}
 
-    def null(self,*params):
-
-        modVals = self.sim(*params)
-
-        return modVals
-
-    def logprob(self,*params):
-
-        modVals = self.sim(*params)
-
-        logModCoiceprob = log(modVals)
-
-        fit = -2*logModCoiceprob
-
-        return fit
 
     def fit(self, sim, mInitialParams):
 
