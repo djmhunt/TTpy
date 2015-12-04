7234483e8adf97a055d38f583fb8dae709a9bf80
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2015-12-04 09:59:17 +0000

    Added a standard Python project metadata file setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+Development Status :: 5 - Production/Stable
+Environment :: Console
+Intended Audience :: Science/Research
+License :: Free for non-commercial use
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python :: 2.7
+Topic :: Scientific/Engineering :: Medical Science Apps
