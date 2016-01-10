b1521eb88fa2eb56486b55e499439c0586e50ff1
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2016-01-10 22:16:03 +0000

    Updated the setup.py file so that it might be useful for anybody installing the code.

--- setup.py
+++ setup.py
@@ -1,9 +1,73 @@
+# -*- coding: utf-8 -*-
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
-Development Status :: 5 - Production/Stable
-Environment :: Console
-Intended Audience :: Science/Research
-License :: Free for non-commercial use
-Natural Language :: English
-Operating System :: OS Independent
-Programming Language :: Python :: 2.7
-Topic :: Scientific/Engineering :: Medical Science Apps
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='sample',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.1.0',
+
+    description='A neurological learning model fitting and simulation package',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://bitbucket.org/djhunt/model-sims',
+
+    # Author details
+    author='Dominic Hunt',
+    author_email='psp01dh@gold.ac.uk',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable'
+
+        'Environment :: Console'
+
+        'Intended Audience :: Science/Research'
+
+        'License :: Free for non-commercial use'
+
+        'Natural Language :: English'
+
+        'Operating System :: OS Independent'
+
+        'Programming Language :: Python :: 2.7'
+
+        'Topic :: Scientific/Engineering :: Medical Science Apps'
+    ],
+
+    # What does your project relate to?
+    keywords='neuroscience modelling Goldsmiths psychology',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib'],
+
+)
+
+
