75351c1f4ac1aeafcf0e3db895b1872e8fc5062f
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2017-06-29 16:59:51 +0100

    Addeda new fitting function BIC2Boot in fitting.fitters.qualityFunc

--- fitting/fitters/qualityFunc.py
+++ fitting/fitters/qualityFunc.py
@@ -5,7 +5,8 @@
 
 from __future__ import division, print_function, unicode_literals, absolute_import
 
-from numpy import log2, sum, ones, exp, shape, amax
+from numpy import log2, sum, ones, exp, shape, amax, array, linspace, concatenate
+from numpy.random import choice
 from collections import Callable
 
 from utils import movingaverage
@@ -17,6 +18,8 @@ def qualFuncIdent(value, **kwargs):
         fitness = value
     elif value == "BIC2":
         fitness = BIC2(**kwargs)
+    elif value == "BIC2Boot":
+        fitness = BIC2Boot(**kwargs)
     elif value == "-2log":
         fitness = logprob
     elif value == "-2AvLog":
@@ -153,5 +156,90 @@ def BIC2(**kwargs):
         return fit
 
     BICfunc.Name = "BIC2"
-    BICfunc.Params = {"numParams": numParams}
+    BICfunc.Params = {"numParams": numParams,
+                      "qualityThreshold": qualityThreshold,
+                      "numActions": numActions,
+                      "randActProb": randActProb}
+    return BICfunc
+
+
+def BIC2Boot(**kwargs):
+    # type : (int, float) -> Callable[[Union[ndarray, list]], float]
+    """
+
+    Parameters
+    ----------
+    numParams : int, optional
+        The number of parameters used by the model used for the fitting process. Default 2
+    qualityThreshold : float, optional
+        The BIC minimum fit quality criterion used for determining if a fit is valid. Default 20.0
+    numActions: int or list of ints the length of the number of trials being fitted, optional
+        The number of actions the participant can choose between for each timestep of the experiment. May need to be
+        specified for each trial if the number of action choices varies between trials. Default 2
+    randActProb: float or list of floats the length of the number of trials being fitted. Optional
+        The prior probability of an action being randomly chosen. May need to be specified for each trial if the number
+        of action choices varies between trials. Default ``1/numActions``
+    numSamples: int, optional
+        The number of samples that will be randomly resampled from ``modVals``. Default 100
+    sampleLen: int, optional
+        The length of the random sample. Default 1
+
+    Returns
+    -------
+
+    """
+
+    # Set the values that will be fixed for the whole fitting process
+    numParams = kwargs.pop("numParams", 2)
+    qualityThreshold = kwargs.pop("qualityThreshold", 20)
+    numActions = kwargs.pop("numActions", 2)
+    randActProb = kwargs.pop("randActProb", 1/numActions)
+    numSamples = kwargs.pop("numSamples", 100)
+    sampleLen = kwargs.pop("sampleLen", 1)
+
+    def BICfunc(modVals):
+        # type: (Union[ndarray, list]) -> float
+        """
+        Generates a fit quality value based on :math:`\mathrm{exp}^{\frac{\mathrm{numParams}\mathrm{log2}\left(\mathrm{numSamples}\right) + \mathrm{BICval}}{\mathrm{BICrandom}} - 1}`
+        The function is a modified version of the Bayesian Informaiton Criterion
+
+        It provides a fit such that when a value is less than one it is a valid fit
+
+        Returns
+        -------
+        fit : float
+            The sum of the model valaues returned
+        """
+
+        sample = array(modVals).squeeze()
+
+        # Calculate the resampled modVals
+        numVals = sample.shape
+        T = max(numVals)
+        probdist = linspace(2 / (T * (T + 1)), 2 / (T + 1), T)
+        choices = choice(range(T), size=numSamples, p=probdist)
+        modValsExtra = array([sample[i: i + sampleLen] for i in choices]).squeeze()
+
+        extendedSample = concatenate((sample, modValsExtra))
+
+        numTrials = shape(extendedSample)
+
+        # We define the Bayesian Information Criteria for the probability of the model given the data, relative a
+        # guessing model
+
+        BICval = numParams * log2(amax(numTrials)) + logprob(extendedSample)
+        BICrandom = logprob(ones(numTrials) * randActProb)
+        qualityConvertor = qualityThreshold**(2/BICrandom)
+
+        fit = qualityConvertor * 2**(BICval/BICrandom - 1)
+
+        return fit
+
+    BICfunc.Name = "BIC2Boot"
+    BICfunc.Params = {"numParams": numParams,
+                      "qualityThreshold": qualityThreshold,
+                      "numActions": numActions,
+                      "randActProb": randActProb,
+                      "numSamples": numSamples,
+                      "sampleLen": sampleLen}
     return BICfunc
