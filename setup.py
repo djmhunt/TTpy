ef21b0d36a777394df133ff9efb0920c90a6b3de
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2020-01-06 14:46:36 +0100

    Updated typo in setup.py and moved requirements to requirements.txt

--- setup.py
+++ setup.py
@@ -60,13 +60,13 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['contrib', 'doc', 'tests']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'pandas', 'XlsxWriter', 'openpyxl'],
+    install_requires='requirements.txt',
 
 )
 
