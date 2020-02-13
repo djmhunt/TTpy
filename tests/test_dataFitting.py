c4f492b4acf3d5c45895ccf7837fb355e51c6ace
Dominic Hunt <2421913+djmhunt@users.noreply.github.com>
2020-02-13 22:41:51 +0000

    Started a unit test file for dataFitting

---
+++ tests/test_dataFitting.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Dominic Hunt
+"""
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import sys
+sys.path.append("../")
+
+import pytest
+import itertools
+import os
+
+import numpy as np
+
+import dataFitting
+import data
+import outputting
+
+@pytest.fixture(scope="session")
+def output_folder(tmpdir_factory):
+
+    folder_name = tmpdir_factory.mktemp("data", numbered=False)
+
+    return folder_name
+
+class TestClass_basic:
+
+    def test_DF_1(self, output_folder, capsys):
+        output_path = str(output_folder)
+
+        with pytest.raises(data.FileError, match='No data files found'):
+            dataFitting.run(output_path=output_path)
