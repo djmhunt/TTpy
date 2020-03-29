a07629090dbda3613d7028b3b7e2614ded355a05
Dominic Hunt <dominic.hunt+github@gmail.com>
2020-03-29 13:34:56 +0100

    Tidied up setup

--- setup.py
+++ setup.py
@@ -5,67 +5,69 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
 
-here = path.abspath(path.dirname(__file__))
+import setuptools
+# To use a consistent encoding
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+short_description = 'Tinker Taylor py is a probabilistic decision-modelling framework for simulating and fitting the '
+short_description += 'responses of people to probabilistic decision making tasks. '
 
-setup(
-    name='Tinker Taylor py',
+setuptools.setup(
+                name='Tinker Taylor py',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0-alpha',
+                # Versions should comply with PEP440.  For a discussion on single-sourcing
+                # the version across setup.py and the project code, see
+                # https://packaging.python.org/en/latest/single_source_version.html
+                version='1.0.0-alpha',
 
-    description='Tinker Taylor py is a probabilistic decision-modelling framework for simulating and fitting the responses of people to probabilistic decison making tasks. ',
-    long_description=long_description,
+                description=short_description,
+                long_description=long_description,
+                long_description_content_type="text/markdown",
 
-    # The project's main homepage.
-    url='https://github.com/djmhunt/TTpy',
+                # The project's main homepage.
+                url='https://github.com/djmhunt/TTpy',
 
-    # Author details
-    author='Dominic Hunt',
-    author_email='d.hunt@gold.ac.uk',
+                # Author details
+                author='Dominic Hunt',
+                author_email='d.hunt@gold.ac.uk',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 5 - Production/Stable'
+                # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+                classifiers=[
+                    'Development Status :: 5 - Production/Stable'
+            
+                    'Environment :: Console'
+            
+                    'Intended Audience :: Science/Research'
+            
+                    'License :: MIT'
+            
+                    'Natural Language :: English'
+            
+                    'Operating System :: OS Independent'
+            
+                    'Programming Language :: Python :: 3'
+            
+                    'Topic :: Scientific/Engineering :: Medical Science Apps'
+                ],
 
-        'Environment :: Console'
+                # What does your project relate to?
+                keywords='neuroscience modelling Goldsmiths psychology PhD Dominic_Hunt',
 
-        'Intended Audience :: Science/Research'
+                # You can just specify the packages manually here if your project is
+                # simple. Or you can use find_packages().
+                packages=setuptools.find_packages(exclude=['contrib', 'doc', 'tests']),
 
-        'License :: MIT'
-
-        'Natural Language :: English'
-
-        'Operating System :: OS Independent'
-
-        'Programming Language :: Python :: 3.7'
-
-        'Topic :: Scientific/Engineering :: Medical Science Apps'
-    ],
-
-    # What does your project relate to?
-    keywords='neuroscience modelling Goldsmiths psychology PhD Dominic_Hunt',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'doc', 'tests']),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires='requirements.txt',
-
+                # List run-time dependencies here.  These will be installed by pip when
+                # your project is installed. For an analysis of "install_requires" vs pip's
+                # requirements files see:
+                # https://packaging.python.org/en/latest/requirements.html
+                install_requires='requirements.txt',
+                python_requires='>=3.7',
 )
