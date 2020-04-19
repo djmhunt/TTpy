5b9de60e219b6f113045fafbf38f134b950f93d5
Dominic Hunt <dominic.hunt+github@gmail.com>
2020-04-19 12:31:58 +0100

    Updated logging tests to use caplog rather than capsys.

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -5,6 +5,7 @@
 import pytest
 import os
 import itertools
+import logging
 
 import simulation
 import outputting
@@ -20,61 +21,81 @@ def output_folder(tmpdir_factory):
 
 class TestClass_basic:
 
-    def test_S_none(self, capsys):
+    def test_S_none(self, caplog):
+        caplog.set_level(logging.INFO)
         simulation.run()
 
-        captured = capsys.readouterr()
-        standard_captured = [c[6:] for c in captured.out.splitlines()]
-        error_captured = captured.err
+        captured = caplog.records
 
-        correct = ['Setup        INFO     {}'.format(outputting.date()),
-                   'Setup        INFO     Log initialised',
-                   'Framework    INFO     Beginning task labelled: Untitled',
-                   "Simulation   INFO     Simulation 0 contains the task Basic: Trials = 100.The model used is QLearn: number_cues = 1, reward_shaper = u'model.modelTemplate.Rewards with Name : model.modelTemplate.Rewards', stimulus_shaper = u'model.modelTemplate.Stimulus with Name : model.modelTemplate.Stimulus', number_critics = 2, prior = array([0.5, 0.5]), expectation = array([[0.5],",
-                   " [0.5]]), decision_function = u'discrete.weightProb with task_responses : 0, 1', beta = 4, actionCode = {0: 0, 1: 1}, non_action = u'None', alpha = 0.3, number_actions = 2.",
-                   'Setup        INFO     Shutting down program']
+        for level in [k.levelname for k in captured]:
+            assert level == 'INFO'
+
+        captured_loggers = [k.name for k in captured]
+        correct_loggers = ['Setup', 'Setup', 'Framework', 'Simulation', 'Setup']
+        for capt, corr in itertools.zip_longest(captured_loggers, correct_loggers):
+            assert capt == corr
+
+        standard_captured = [k.message.replace('\n', '') for k in captured]
+        correct = ['{}'.format(outputting.date()),
+                   'Log initialised',
+                   'Beginning task labelled: Untitled',
+                   "Simulation 0 contains the task Basic: Trials = 100.The model used is QLearn: number_actions = 2, number_cues = 1, number_critics = 2, prior = array([0.5, 0.5]), non_action = 'None', actionCode = {0: 0, 1: 1}, stimulus_shaper = 'model.modelTemplate.Stimulus with ', reward_shaper = 'model.modelTemplate.Rewards with ', decision_function = 'discrete.weightProb with task_responses : 0, 1', alpha = 0.3, beta = 4, expectation = array([[0.5],       [0.5]]).",
+                   'Shutting down program']
 
         for correct_line, standard_captured_line in itertools.zip_longest(correct, standard_captured):
             assert standard_captured_line == correct_line
 
-    def test_S_folder(self, output_folder, capsys):
+    def test_S_folder(self, output_folder, caplog):
+        caplog.set_level(logging.INFO)
         output_path = str(output_folder).replace('\\', '/')
         simulation.run(output_path=output_path)
 
-        captured = capsys.readouterr()
-        standard_captured = [c[6:] for c in captured[0].splitlines()]
-        error_captured = captured[1]
+        captured = caplog.records
 
-        correct = ['Setup        INFO     {}'.format(outputting.date()),
-                   'Setup        INFO     Log initialised',
-                   'Framework    INFO     Beginning task labelled: Untitled',
-                   "Simulation   INFO     Simulation 0 contains the task Basic: Trials = 100.The model used is QLearn: number_cues = 1, reward_shaper = u'model.modelTemplate.Rewards with Name : model.modelTemplate.Rewards', stimulus_shaper = u'model.modelTemplate.Stimulus with Name : model.modelTemplate.Stimulus', number_critics = 2, prior = array([0.5, 0.5]), expectation = array([[0.5],",
-                   " [0.5]]), decision_function = u'discrete.weightProb with task_responses : 0, 1', beta = 4, actionCode = {0: 0, 1: 1}, non_action = u'None', alpha = 0.3, number_actions = 2.",
-                   'Setup        INFO     Shutting down program']
+        for level in [k.levelname for k in captured]:
+            assert level == 'INFO'
+
+        captured_loggers = [k.name for k in captured]
+        correct_loggers = ['Setup', 'Setup', 'Framework', 'Simulation', 'Setup']
+        for capt, corr in itertools.zip_longest(captured_loggers, correct_loggers):
+            assert capt == corr
+
+        standard_captured = [k.message.replace('\n', '') for k in captured]
+        correct = ['{}'.format(outputting.date()),
+                   'Log initialised',
+                   'Beginning task labelled: Untitled',
+                   "Simulation 0 contains the task Basic: Trials = 100.The model used is QLearn: number_actions = 2, number_cues = 1, number_critics = 2, prior = array([0.5, 0.5]), non_action = 'None', actionCode = {0: 0, 1: 1}, stimulus_shaper = 'model.modelTemplate.Stimulus with ', reward_shaper = 'model.modelTemplate.Rewards with ', decision_function = 'discrete.weightProb with task_responses : 0, 1', alpha = 0.3, beta = 4, expectation = array([[0.5],       [0.5]]).",
+                   'Shutting down program']
 
         for correct_line, standard_captured_line in itertools.zip_longest(correct, standard_captured):
             assert standard_captured_line == correct_line
 
         assert os.listdir(output_path) == []
 
-    def test_S_label(self, output_folder, capsys):
+    def test_S_label(self, output_folder, caplog):
+        caplog.set_level(logging.INFO)
         output_path = str(output_folder).replace('\\', '/')
-        simulation.run(label='test', output_path=output_path)
-
-        captured = capsys.readouterr()
-        standard_captured = [c[6:].strip() for c in captured[0].splitlines()]
-        error_captured = captured[1]
         date = outputting.date()
 
-        correct = ['Setup        INFO     {}'.format(date),
-                   'Setup        INFO     Log initialised',
-                   'Setup        INFO     The log you are reading was written to {}/Outputs/test_{}/log.txt'.format(output_path, date),
-                   'Framework    INFO     Beginning task labelled: test',
-                   "Simulation   INFO     Simulation 0 contains the task Basic: Trials = 100.The model used is QLearn: number_cues = 1, reward_shaper = u'model.modelTemplate.Rewards with Name : model.modelTemplate.Rewards', stimulus_shaper = u'model.modelTemplate.Stimulus with Name : model.modelTemplate.Stimulus', number_critics = 2, prior = array([0.5, 0.5]), expectation = array([[0.5],",
-                   "[0.5]]), decision_function = u'discrete.weightProb with task_responses : 0, 1', beta = 4, actionCode = {0: 0, 1: 1}, non_action = u'None', alpha = 0.3, number_actions = 2.",
-                   'Framework    INFO     Beginning simulation output processing',
-                   'Framework    INFO     Store data for simulation 0',
-                   'Setup        INFO     Shutting down program']
+        simulation.run(label='test', output_path=output_path)
+
+        captured = caplog.records
+
+        for level in [k.levelname for k in captured]:
+            assert level == 'INFO'
+
+        captured_loggers = [k.name for k in captured]
+        correct_loggers = ['Setup', 'Setup', 'Setup', 'Framework', 'Simulation', 'Setup']
+        for capt, corr in itertools.zip_longest(captured_loggers, correct_loggers):
+            assert capt == corr
+
+        standard_captured = [k.message.replace('\n', '') for k in captured]
+        correct = ['{}'.format(date),
+                   'Log initialised',
+                   'The log you are reading was written to {}/Outputs/test_{}/log.txt'.format(output_path, date),
+                   'Beginning task labelled: test',
+                   "Simulation 0 contains the task Basic: Trials = 100.The model used is QLearn: number_actions = 2, number_cues = 1, number_critics = 2, prior = array([0.5, 0.5]), non_action = 'None', actionCode = {0: 0, 1: 1}, stimulus_shaper = 'model.modelTemplate.Stimulus with ', reward_shaper = 'model.modelTemplate.Rewards with ', decision_function = 'discrete.weightProb with task_responses : 0, 1', alpha = 0.3, beta = 4, expectation = array([[0.5],       [0.5]]).",
+                   'Shutting down program']
 
         for correct_line, standard_captured_line in itertools.zip_longest(correct, standard_captured):
             assert standard_captured_line == correct_line
@@ -87,7 +108,11 @@ class TestClass_basic:
         assert not os.path.exists(folder_path + 'Pickle')
 
         with open(folder_path + 'log.txt') as log:
-            cleaned_log = [l[6:].strip() if l[0] == ' ' else l[15:].strip() for l in log.readlines()]
+            cleaned_log = [l.split('    ')[-1].strip() for l in log.readlines()]
+            correct[-2] = correct[-2][:-15]
+            final_correct = correct[-1]
+            correct[-1] = '[0.5]]).'
+            correct.append(final_correct)
         for correct_line, standard_captured_line in itertools.zip_longest(correct, cleaned_log):
             assert standard_captured_line == correct_line
 
